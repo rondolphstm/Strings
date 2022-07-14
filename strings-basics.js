6fe09e93ab3d64e42339a2208ade6c11a91cfd93
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-07-14 11:41:36 -0400

    strings info

---
+++ strings-basics.js
@@ -0,0 +1,22 @@
+const message = 'Hello javascript students'
+const msgToLowerCase = message.toLowerCase()
+const msgToUpperCase = message.toUpperCase()
+const messageNoSpaces = message.trim()
+const newMessage = message.replace('student','engineers')
+const withSubString = message.substring(9, 14)
+message.startsWith('Hello')
+
+// console.log(message)
+// console.log(msgToLowerCase)
+// console.log(msgToUpperCase)
+// console.log(messageNoSpaces)
+// console.log(newMessage)
+// console.log(withSubString)
+// console.log(message.startsWith('Hello'))
+// console.log(message.endsWith('Students'))
+
+
+const myString ='Hello level up students!'
+const newString = myString.split('-')
+console.log(newString)
+
